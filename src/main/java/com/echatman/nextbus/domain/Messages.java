e469dbd3692d364bd58d250322e51d17ebf2e88f
Elizabeth Chatman <elizabeth.a.chatman@gmail.com>
2014-02-01 07:58:08 -0800

    Removing unused imports

--- src/main/java/com/echatman/nextbus/domain/Messages.java
+++ src/main/java/com/echatman/nextbus/domain/Messages.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlID;
-import javax.xml.bind.annotation.XmlIDREF;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
